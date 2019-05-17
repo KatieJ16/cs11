ce22e28ccc9521553c5babe915a138e2a697039a
K Johnston <kjohnst@sandia.gov>
2019-05-16 23:48:32 -0700

    update with file update

--- hw2/convert.cpp
+++ hw2/convert.cpp
@@ -54,7 +54,7 @@ int main(){
     UValue output = u.convert_to(input, to_units);
     cout << "Converted to: " << output.get_value() << " " << output.get_units() << "\n";
   }
-  catch(int e){ //when you can't convert
+  catch (const std::invalid_argument& ia){ //when you can't convert
     cout << "Couldn't convert to " << to_units <<"!\n";
 
   }
